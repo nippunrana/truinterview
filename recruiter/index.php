3ae7bd2f9bc92e2aa99e1962f26ed65961a51428
Nippun Rana <12187730+nippunrana@users.noreply.github.com>
2026-06-12 14:44:12 +0000

    feat: add recruiter portal badge and implement teal-emerald design system variables

--- recruiter/index.php
+++ recruiter/index.php
@@ -152,6 +152,7 @@ $firstName = !empty($words[0]) ? $words[0] : 'Recruiter';
           <path stroke-linecap="round" stroke-linejoin="round" d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
         </svg>
         <span class="brand-title">TruInterview</span>
+        <span class="portal-badge">Recruiter</span>
       </a>
       
       <div class="user-nav">
